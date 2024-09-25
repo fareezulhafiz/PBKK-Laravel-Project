1c42171443b1d7ea15ed7d3684369ce178e52cbe
fareezulhafiz <fareezulhafiz15@gmail.com>
2024-09-25 14:59:30 +0700

    Project Update (Week 5)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'username' =>fake()->unique()->username(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
@@ -35,6 +36,7 @@ class UserFactory extends Factory
     /**
      * Indicate that the model's email address should be unverified.
      */
+
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +19,37 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // Category::create([
+        //     'name' => 'Web Design',
+        //     'slug' => 'web-design',
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Artikel 1',
+        //     'author_id' => 1,
+        //     'category_id' => 1,
+        //     'slug' => 'judul-artikel-1',
+        //     'body' => 'Laravel includes the ability to seed 
+        //     your database with data using seed classes. All 
+        //     seed classes are stored in the database/seeders 
+        //     directory. By default, a DatabaseSeeder class is 
+        //     defined for you. From this class, you may use the 
+        //     call method to run other seed classes, allowing 
+        //     you to control the seeding order.'
+        // ]);
+
+        $this->call([CategorySeeder::class, UserSeeder::class]);
+
+        Post::factory(100)->recycle([
+            
+            Category::all(),
+            User::all(),
+
+        ])->create();
     }
 }
